c7be67097f0d31dce4afc753eaf8065cf5462cb2
Srinath Silla <sillasrinath@gmail.com>
2024-11-10 03:48:02 -0500

    Add MatchesList component

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,41 @@ const ProfileSelector = () => (
   </div>
 );
 
+const MatchesList = () => (
+  <div className="rounded-lg shadow-lg p-4">
+    <h2 className="text-2xl font-bold mb-4">Matches</h2>
+    <ul>
+      {[
+        {
+          id: 1,
+          firstName: "Foo",
+          lastName: "Bar",
+          imageUrl:
+            "http://localhost:8080/images/0e985a4b-6944-4dec-b0f7-4a4453592dd0.jpg",
+        },
+        {
+          id: 2,
+          firstName: "Baz",
+          lastName: "Qux",
+          imageUrl:
+            "http://localhost:8080/images/0e985a4b-6944-4dec-b0f7-4a4453592dd0.jpg",
+        },
+      ].map((match) => (
+        <li key={match.id} className="mb-2">
+          <button className="w-full rounded hover:bg-gray-100 item-center flex">
+            <img src={match.imageUrl} className="w-16 h-16 rounded-full mr-3 object-cover" />
+            <span>
+              <h3 className="font-bold"> 
+                {match.firstName} {match.lastName}
+              </h3>
+            </span>
+          </button>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
 function App() {
 
   return (
@@ -37,6 +72,7 @@ function App() {
           <MessageCircle />
         </nav>
         <ProfileSelector/>
+        {/* <MatchesList/> */}
       </div>
     </>
   );
